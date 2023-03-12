77d2f13569a799dafb9cf07f76ede537af9e3fd9
DmitryForman <masterdemion@gmail.com>
2023-03-12 23:07:25 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitry Yudin");
+        System.out.println("12.09.1989");
+    }
+}
